c76cbc6b377dd28c87f434d81772d535407fb2db
gonnajoong <gonnajoong@gmail.com>
2021-05-13 13:31:35 +0900

    위치기반 일기예보 api 연동

--- Utils/constant.js
+++ Utils/constant.js
@@ -1,8 +1,5 @@
 const Constant = [{
-    KEY : {
-        weatherEncoding: 'RTVdaxmbOhSgBbbnOhsN5sK6S4UF07LLo8UGNl0EazSqNFw3rUTtJfGjFyIVfEaIMzwiwZEjaQwwuV5D4ccsYA%3D%3D',
-        weatherDecoding: 'RTVdaxmbOhSgBbbnOhsN5sK6S4UF07LLo8UGNl0EazSqNFw3rUTtJfGjFyIVfEaIMzwiwZEjaQwwuV5D4ccsYA=='
-    }
+    KEY : '8347650a0034f9608ff315324c80721b',
 }];
 
 export default Constant;
--- Views/Main.js
+++ Views/Main.js
@@ -6,23 +6,27 @@ import Geolocation from "react-native-geolocation-service";
 import main from '../Assets/views/_main';
 import axios from "axios";
 
-// import {result} from '../server/api/v1/river/get';
-
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             searchText: '',
-            currPos: {latitude: 0.0, longitude: 0.0}, // 최초 좌표
+            lat: 0,
+            lon: 0,
             riverTemp: '',
+            weatherType: '', //날씨 상태
+            temp_max: '',
+            temp_min: '',
+            temp: '',
+            s_temp: '', // 체감 온도
         };
     }
     async componentDidMount() {
         await this.requestLocationPermission();
-        this.getsLocation();
+        await this.getsLocation();
         await this.getRiverTemp();
-        // await this.setState({riverTemp: result});
-        // console.log('console.log '+result);
+        await this.getNeighborhoodTemp(this.state.lat, this.state.lon);
+
     }
 
     handler = (e, key) => {
@@ -31,9 +35,10 @@ class Main extends Component {
         this.setState(temp);
     }
     
-    getsLocation = () => {
-        Geolocation.getCurrentPosition( (position) => {
-            this.setState({currPos: position.coords});
+    getsLocation = async () => {
+        await Geolocation.getCurrentPosition( async (position) => {
+
+            await this.setState({lat: position.coords.latitude, lon: position.coords.longitude});
         }, (error) => {
             alert('error : ' + error.message);
             //퍼미션 없을 시 실패 AndroidManifest.xml 에서 추가할 것
@@ -44,25 +49,46 @@ class Main extends Component {
         await axios.get('https://api.qwer.pw/request/hangang_temp?apikey=guest').then(
             async (response) => {
                 let data = response['data'];
-
-                // for(let key in response) {
-                //     console.log('객체배열 ' + response + ' 구분선 ' + key);
-                //     console.log(response[key]);
-                // }
-                console.log('객체 ' + data[0].result);
                 if(data[0].result == 'success') {
-                    console.log('성공');
                     let riverTemp = {riverTemp: data[1].respond.temp};
-
                     await this.setState(riverTemp);
                 } else {
-                    console.log('실패');
                     alert('수온 요청 실패');
                 }
             }
         ).catch(function (error){
             console.log('에라 ' + error);
         });
+    }
+
+    async getNeighborhoodTemp (lat, lon) {
+        let getUrl = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=8347650a0034f9608ff315324c80721b'+'&lang=kr';
+        console.log('주소 - '+ getUrl);
+        await axios.get(getUrl).then(
+            async (response) => {
+                let data = response['data'];
+                console.log('날씨 - ' + data.name);
+                alert('지역명' + data.name);
+                let weather = data.weather;
+                let main = data.main;
+                await this.setState({
+                    weatherType: weather[0].description, //날씨 상태
+                    temp_max: this.Kconvert(main.temp_max),
+                    temp_min: this.Kconvert(main.temp_min),
+                    temp: this.Kconvert(main.temp),
+                    s_temp: 0, // 체감 온도
+                });
+            }
+        ).catch(function (error){
+            console.log('에라 ' + error);
+        });
+    }
+
+    Kconvert (kelvin) {
+        let kelvinZero = parseFloat(273.15);
+        let celsius = 0;
+        celsius = parseFloat(kelvin - kelvinZero);
+        return celsius.toFixed(1);
     }
     
     async requestLocationPermission () {
@@ -71,9 +97,7 @@ class Main extends Component {
         if(!granted) {
             try {
                 //퍼미션 요청 다이얼로그 보이기
-                if(granted == PermissionsAndroid.RESULTS.GRANTED) {
-                    alert('위치정보 사용을 허가하셨습니다.');
-                } else {
+                if(granted != PermissionsAndroid.RESULTS.GRANTED) {
                     alert('위치정보 사용을 거부하셨습니다.\n앱의 기능이 제한됩니다.');
                 }
             } catch (err) {
@@ -85,7 +109,7 @@ class Main extends Component {
 
 
     render() {
-        const {searchText, currPos, riverTemp} = this.state;
+        const {searchText, riverTemp, weatherType, temp_max, temp_min, temp, s_temp} = this.state;
         return (
             <View style={main.container}>
                 <View style={main.searchWrap}>
@@ -105,12 +129,12 @@ class Main extends Component {
                             <Image
                                 style={main.weatherIcon}
                             />
-                            <Text style={main.weatherTemp}>26°</Text>
+                            <Text style={main.weatherTemp}>{temp ? temp : '-'}°</Text>
                         </View>
                         <View style={main.weatherTempRightWrap}>
-                            <Text style={main.weatherTempDetailText}>맑음</Text>
-                            <Text style={main.weatherTempDetailText}>27° / 14°</Text>
-                            <Text style={main.weatherTempDetailText}>체감온도 23°</Text>
+                            <Text style={main.weatherTempDetailText}>{weatherType ? weatherType : '-'}</Text>
+                            <Text style={main.weatherTempDetailText}>{temp_max ? temp_max : '-'}° / {temp_min ? temp_min : '-'}°</Text>
+                            <Text style={main.weatherTempDetailText}>체감온도 {s_temp ? s_temp : '-'}°</Text>
                         </View>
                     </View>
                     <View style={main.weatherRefreshWrap}>
@@ -121,7 +145,6 @@ class Main extends Component {
                     </View>
                     <Text style={main.weatherFavoritText}>
                         따듯한 한강 주변 지역
-                        / 위도 : {currPos.latitude} 경도 : {currPos.longitude}
                     </Text>
                 </View>
                 <View style={main.riverTempWrap}>
